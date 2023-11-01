8f984412b6c096f645b5ec77d224af2c9b882ebc
Hefero <guilherme.marques00@hotmail.com>
2023-11-01 12:03:23 -0300

    pi

--- aula5/stop.cpp
+++ aula5/stop.cpp
@@ -1,9 +1,7 @@
-//pwm1.cpp
-//compila pwm1 -c -w
 #include "MotorH/MotorH.cpp"
 #include <wiringPi.h>
 #include <softPwm.h>
 int main () {
     motorH motorH;
-    motorH.turnLeft();
+        motorH.stop();
 }
