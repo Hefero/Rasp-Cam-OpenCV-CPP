34bda23891bb459c439f9f144a367262e18477ad
Hefero <guilherme.marques00@hotmail.com>
2023-11-02 10:51:51 -0300

    a

--- aula5/send_video.cpp
+++ aula5/send_video.cpp
@@ -43,6 +43,7 @@ int main(int argc, char** argv)
             string command;
             int bytesCommand = rec.recvString(command);
             if(command.size() > 1){                
+                std::cout << command << std::endl;
                 motor.execute(command);
             }            
         }
