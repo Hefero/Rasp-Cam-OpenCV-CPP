ee60f22144b9fa720eb045ea23391789f18a74d9
Hefero <guilherme.marques00@hotmail.com>
2023-11-02 10:39:27 -0300

    cc

--- aula5/MotorH/MotorH.cpp
+++ aula5/MotorH/MotorH.cpp
@@ -109,10 +109,12 @@ void motorH::motorEsq(int command, int duty)
 
 void motorH::execute(string command)
 {
-    std::cout << "receive command" << command << std::endl;
-    char b2[4096] = "b2";    
-    if( strcmp(b2,command.data()) == 0 ){
-        std::cout << command << std::endl;
+    if(command.size() > 1){
+        std::cout << "receive command " << command << std::endl;
+        char b2[4096] = "b2";    
+        if( strcmp(b2,command.data()) == 0 ){
+            std::cout << command << std::endl;
+        }
     }
 }
 
