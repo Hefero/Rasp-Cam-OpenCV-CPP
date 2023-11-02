c8fea5c215ee22994535ef8870cbe8bd1ae23738
Hefero <guilherme.marques00@hotmail.com>
2023-11-02 12:10:08 -0300

    a

--- aula5/receive_video_gui.cpp
+++ aula5/receive_video_gui.cpp
@@ -78,34 +78,36 @@ auto end = std::chrono::high_resolution_clock::now();
 
 void sendCommand(Receiver& rec, Mygui& gui)
 {
-    if (gui.mouse.up){
+    //if (gui.mouse.up){
         rec.sendString("stop");
-    }
-    if(!gui.b1.getState()){
+    //}
+    /*
+    if(gui.b1.getState()){
         rec.sendString("b1");
     }
-    if(!gui.b2.getState()){
+    if(gui.b2.getState()){
         rec.sendString("b2");
     }
-    if(!gui.b3.getState()){
+    if(gui.b3.getState()){
         rec.sendString("b3");        
     }
-    if(!gui.b4.getState()){
+    if(gui.b4.getState()){
         rec.sendString("b4");        
     }
-    if(!gui.b5.getState()){
+    if(gui.b5.getState()){
         rec.sendString("b5");        
     }
-    if(!gui.b6.getState()){
+    if(gui.b6.getState()){
         rec.sendString("b6");        
     }
-    if(!gui.b7.getState()){
+    if(gui.b7.getState()){
         rec.sendString("b7");        
     }
-    if(!gui.b8.getState()){
+    if(gui.b8.getState()){
         rec.sendString("b8");        
     }
-    if(!gui.b9.getState()){
+    if(gui.b9.getState()){
         rec.sendString("b9");        
     }
+    */
 }
