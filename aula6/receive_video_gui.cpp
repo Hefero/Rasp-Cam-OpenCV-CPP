0d43f2172d6e14aadb872e9e2d52f3fecf5c6841
Hefero <guilherme.marques00@hotmail.com>
2023-11-12 15:11:29 -0300

    a

--- aula6/receive_video_gui.cpp
+++ aula6/receive_video_gui.cpp
@@ -7,7 +7,11 @@
 
 Mygui gui;
 
+CascadeClassifier cascade;
+
+void detectAndDisplay( Mat& frame, std::vector<Rect>& faces );
 void sendCommand(Receiver& rec, Mygui& gui);
+void sendFollow(Receiver& rec, Mat& frame, std::vector<Rect>& faces);
 void onMouseGui(int event, int x, int y, int flags, void* userdata)
 { 
     Mouse* mouse=(Mouse*)userdata;
@@ -25,8 +29,10 @@ int main(int argc, char** argv)
 Receiver rec(argc, argv);
 
 Mat img = Mat::zeros(480 , 640, CV_8UC3);
+std::vector<Rect> faces;
 vector<unsigned char> compressed;
 Mat concatImg = Mat::zeros(480+480 , 640, CV_8UC3);
+cascade.load("haar.xml");
 
 namedWindow("janela");
 setMouseCallback("janela", onMouseGui, &gui.mouse);
@@ -39,19 +45,73 @@ auto end = std::chrono::high_resolution_clock::now();
  while (key != 'q') {
     try{
         gui.guiLoop();
-        sendCommand(rec, gui);
+        //sendCommand(rec, gui);
         if (rec.recvBytes(compressed) > 0){
             img = imdecode(compressed,1);
         }
-        rec = Receiver(argc, argv);
-        concatImg = grudaH(gui.a,img);        
-        imshow("janela",concatImg);        
+
+        //detectAndDisplay(img, faces);
+        sendFollow(rec, img, faces);
+        //concatImg = grudaH(gui.a,img);        
+        imshow("janela",img);
     }
     catch(cv::Exception ex){
             continue;
     }
     if (key = cv::waitKey(1) >= 0) break;
  }
+}
+
+void detectAndDisplay( Mat& frame, std::vector<Rect>& faces )
+{
+    Mat frame_gray;
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    equalizeHist( frame_gray, frame_gray );
+    //-- Detect faces    
+    Size minSize=Size(3,3);
+    Size maxSize=Size(600,600);
+    cascade.detectMultiScale( frame_gray, faces, 1.1, 3, 0, minSize, maxSize);
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4 );
+        Mat faceROI = frame_gray( faces[i] );
+        //-- In each face, detect eyes
+        cv::putText(frame, //target image
+            to_string(faces[i].width), //text
+            cv::Point(faces[i].x, faces[i].y), //top-left position
+            cv::FONT_HERSHEY_DUPLEX,
+            1.0,
+            CV_RGB(118, 185, 0), //font color
+            2);
+
+    }
+    //-- Show what you got
+    //imshow( "Capture - Face detection", frame );
+}
+
+void sendFollow(Receiver& rec, Mat& frame, std::vector<Rect>& faces)
+{
+    if (30 < faces[0].width < 100){
+        int Xrect = faces[0].x + faces[0].width/2;
+        int Xcenter = 240;
+        int epsilon = 10;
+
+        if ((Xrect - Xcenter) < epsilon){ // está ao centro
+            //rec.sendString("b2");
+        }
+        else{
+            if (Xrect > (Xcenter + epsilon)){ // está a direita
+              //rec.sendString("b1");
+            }
+            if (Xrect < (Xcenter - epsilon)){ // está a esquerda
+              //rec.sendString("b7");
+            }
+        }
+    }
+    else {
+        rec.sendString("stop");
+    }
 }
 
 void sendCommand(Receiver& rec, Mygui& gui)
